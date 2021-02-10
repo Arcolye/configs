37af6924dc221a6628407dd8481a851bbf32429b
Stephen Hunter <stephen.hunter@mindbodyonline.com>
2021-02-10 17:45:42 -0600

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Fira Code Font
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
+
+brew install --cask firefox
+
+
+cp ./.gitconfig ~
